56163739dc6927a0001ca8f4111529798a7f0b00
AryanPiash <aryanpiash420@gmail.com>
2022-05-26 13:26:56 +0600

    added top brands section on home page

---
+++ src/components/Home/ProductBrand.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const ProductBrand = () => {
+    return (
+        <div>
+            <h1 className='text-4xl text-center font-bold mt-36 mb-16'>AUTOPARTS'S <span className='text-primary'>BRANDS</span></h1>
+            <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 px-12 my-12'>
+                <img src="https://cdn.shopify.com/s/files/1/0074/7887/0105/files/br151_214x128.jpg?v=1640834859" alt="" />
+                <div>
+                    <img src="https://cdn.shopify.com/s/files/1/0074/7887/0105/files/br154_214x128.jpg?v=1640834963" alt="" />
+                </div>
+                <img src="https://cdn.shopify.com/s/files/1/0074/7887/0105/files/br157_214x128.jpg?v=1640835003" alt="" />
+                <img src="https://cdn.shopify.com/s/files/1/0074/7887/0105/files/br1591_214x128.jpg?v=1640835052" alt="" />
+                <img src="https://cdn.shopify.com/s/files/1/0074/7887/0105/files/br1594_214x128.jpg?v=1640835214" alt="" />
+                <img src="https://cdn.shopify.com/s/files/1/0074/7887/0105/files/br1597_214x128.jpg?v=1640835270" alt="" />
+                <img src="https://cdn.shopify.com/s/files/1/0074/7887/0105/files/br152_214x128.jpg?v=1640834939" alt="" />
+                <img src="https://cdn.shopify.com/s/files/1/0074/7887/0105/files/br155_214x128.jpg?v=1640834976" alt="" />
+                <img src="https://cdn.shopify.com/s/files/1/0074/7887/0105/files/br158_214x128.jpg?v=1640835019" alt="" />
+                <img src="https://cdn.shopify.com/s/files/1/0074/7887/0105/files/br1592_214x128.jpg?v=1640835178" alt="" />
+                <img src="https://cdn.shopify.com/s/files/1/0074/7887/0105/files/br1595_214x128.jpg?v=1640835232" alt="" />
+                <img src="https://cdn.shopify.com/s/files/1/0074/7887/0105/files/br153_214x128.jpg?v=1640834950" alt="" />
+                <img src="https://cdn.shopify.com/s/files/1/0074/7887/0105/files/br156_214x128.jpg?v=1640834989" alt="" />
+                <img src="https://cdn.shopify.com/s/files/1/0074/7887/0105/files/br1593_214x128.jpg?v=1640835197" alt="" />
+                <img src="https://cdn.shopify.com/s/files/1/0074/7887/0105/files/br1596_214x128.jpg?v=1640835254" alt="" />
+                <img src="https://cdn.shopify.com/s/files/1/0074/7887/0105/files/br1598_214x128.jpg?v=1640835288" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default ProductBrand;
